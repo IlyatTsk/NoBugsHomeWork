387aff47ccc8e7d1aca5a85220b6d5ec9e8ba2be
Ilya <ipopov34@gmail.com>
2025-04-19 23:21:53 +0700

    upd project

---
+++ src/main/java/multithreading/Task2.java
@@ -0,0 +1,32 @@
+package multithreading;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        Thread threadA = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("A");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread threadB = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("B");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+    }
+}
---
+++ src/main/java/multithreading/Task4.java
@@ -0,0 +1,39 @@
+package multithreading;
+
+public class Task4 {
+
+    public static class Counter {
+
+        public static int counter = 0;
+
+        public static int getCounter() {
+            return counter;
+        }
+
+        public synchronized static void increment() {
+            counter++;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                Counter.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                Counter.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+        System.out.println("Итоговое значение счетчика: " + Counter.getCounter());
+    }
+}
